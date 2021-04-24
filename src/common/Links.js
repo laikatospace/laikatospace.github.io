1a2ee78150c80cc4386d8940c45db15e32e0d464
laikatospace <83052904+laikatospace@users.noreply.github.com>
2021-04-24 18:43:01 -0400

    feex a bit

--- src/common/Links.js
+++ src/common/Links.js
@@ -1,7 +1,7 @@
 export const buyLink = 'https://exchange.pancakeswap.finance/#/swap?outputCurrency=0x155e80f27D2fb89e74130D6432A48a035D8D1103';
 export const telegramLink = 'https://t.me/laikaprotocol';
 export const twitterLink = 'https://twitter.com/laikaprotocol';
-export const githubLink = 'https://github.com';
+export const githubLink = 'https://github.com/laikatospace';
 export const bscLink = 'https://bscscan.com/address/0x155e80f27D2fb89e74130D6432A48a035D8D1103';
 export const mediumLink = 'https://medium.com';
 export const redditLink = 'https://reddit.com';
